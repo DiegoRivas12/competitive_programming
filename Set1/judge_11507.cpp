e6485a42003ad8231b0f159b658782bd11e2b1b2
Diego Rivas <drivash@unsa.edu.pe>
2022-10-11 19:20:01 -0500

    judge_11507

--- Set1/judge_11507.cpp
+++ Set1/judge_11507.cpp
@@ -1,38 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-struct Punto;
-int main(){
-
-    int L;
-    
-    vector<string> respuestas;
-    int i=0;
-    while(true){
-        cin>>L;
-        if(L==0){
-            break;
-        }
-        string instruccion;
-        vector<string> instruccionCaso;
-        cout<<"xd"<<endl;
-        while(cin>>instruccion){
-            //instruccionCaso.insert(instruccionCaso.begin(),instruccion);
-            if(instruccion[instruccion.size()-1]=='\0'){break;}
-               //break;
-            instruccionCaso.push_back(instruccion);
-            //cout<<instruccionCaso[0]<<endl;
-        }
-        cout<<"Caso "<<i<<endl;
-        for(int j=0;j<instruccionCaso.size();j++){
-            cout<<instruccionCaso[i]<<endl;
-        }
-        i++;
-    }
-
-    return 0;
-}
-
+// Rotaciones: https://www.colegioconcepcionsanpedro.cl/wp-content/uploads/2020/07/GEOMETRIA-4%C2%BAC-G-NORAMBUENA-6-al-10-de-julio.pdf
 struct Punto{
 
     int x;int y;int z;
@@ -41,5 +10,121 @@ struct Punto{
         this->y=y;
         this->z=z;
     }
+    Punto(){
+
+    }
 
 };
+void rotarY90(Punto,Punto&,char);
+void rotarZ90(Punto,Punto&,char);
+int main(){
+
+    int L;
+    int i=0;
+    while(true){
+        cin>>L;
+        if(L==0){
+            break;
+        }
+        //Creando Puntos de 0 hasta n
+        Punto alambre[L+1]={};
+        alambre[0].x=0;alambre[0].y=0;alambre[0].z=0;
+        for(int p=1;p<=L;p++){
+            alambre[p].x=p;alambre[p].y=0;alambre[p].z=0;
+        }
+        /*cout<<"Puntos Iniciales "<<i<<endl;
+        for(int j=0;j<=L;j++){
+            cout<<alambre[j].x<<" "<<alambre[j].y<<" "<<alambre[j].z<<endl;
+        }*/
+
+        string instruccion;
+    
+        //cout<<"xd"<<L<<endl;
+        for(int j=L-1;j>0;j--){//Recepcion de instrucciones
+            cin>>instruccion;
+            //instruccionCaso.insert(instruccionCaso.begin(),instruccion);
+            //instruccionCaso.push_back(instruccion);
+            //cout<<instruccionCaso[j]<<endl;
+            for(int k=j+1;k<=L;k++){//Des desde el punto de referencia rotaremos todos los puntos que estan a su derecha
+                char signo=instruccion[0];
+                if(instruccion[1]=='y'){
+                    rotarY90(alambre[j],alambre[k],signo);//Punto puntoReferencial,Punto& puntoRotado
+                }
+                else if(instruccion[1]=='z'){
+                    rotarZ90(alambre[j],alambre[k],signo);//Punto puntoReferencial,Punto& puntoRotado
+                }
+                else{//No
+                    continue;
+                }
+            }
+        }
+        /*cout<<"Puntos finales "<<i<<endl;
+        for(int j=0;j<=L;j++){
+            cout<<alambre[j].x<<" "<<alambre[j].y<<" "<<alambre[j].z<<endl;
+        }*/
+        //Respuesta
+        string respuesta;
+        int x=alambre[L].x-alambre[L-1].x;
+        int y=alambre[L].y-alambre[L-1].y;
+        int z=alambre[L].z-alambre[L-1].z;
+        if(x==1 && y==0 && z==0){
+            cout<<"+x"<<endl;
+        }
+        else if(x==-1 && y==0 && z==0){
+            cout<<"-x"<<endl;
+        }
+        else if(x==0 && y==1 && z==0){
+            cout<<"+y"<<endl;
+        }
+        else if(x==0 && y==-1 && z==0){
+            cout<<"-y"<<endl;
+        }
+        else if(x==0 && y==0 && z==1){
+            cout<<"+z"<<endl;
+        }
+        else if(x==0 && y==0 && z==-1){
+            cout<<"-z"<<endl;
+        }
+        //cout<<
+        i++;
+    }
+
+    return 0;
+}
+
+
+void rotarY90(Punto puntoReferencial,Punto& puntoRotado,char signo){
+    int x=puntoRotado.x-puntoReferencial.x;
+    int y=puntoRotado.y-puntoReferencial.y;
+    int z=puntoRotado.z-puntoReferencial.z;
+    if(signo=='-'){// (x,y) -> (y,-x)
+        puntoRotado.x=y;
+        puntoRotado.y=-x;
+    }
+    else if(signo=='+'){// (x,y) -> (-y,x)
+        puntoRotado.x=-y;
+        puntoRotado.y=x;
+    }
+
+    //Sumamos el punto referencial
+    puntoRotado.x=puntoRotado.x+puntoReferencial.x;
+    puntoRotado.y=puntoRotado.y+puntoReferencial.y;
+    puntoRotado.z=puntoRotado.z+puntoReferencial.z;
+}
+void rotarZ90(Punto puntoReferencial,Punto& puntoRotado,char signo){
+    int x=puntoRotado.x-puntoReferencial.x;
+    int y=puntoRotado.y-puntoReferencial.y;
+    int z=puntoRotado.z-puntoReferencial.z;
+    if(signo=='-'){// (x,z) -> (z,-x)
+        puntoRotado.x=z;
+        puntoRotado.z=-x;
+    }
+    else if(signo=='+'){// (x,z) -> (-z,x)
+        puntoRotado.x=-z;
+        puntoRotado.z=x;
+    }
+    //Sumamos el punto referencial
+    puntoRotado.x=puntoRotado.x+puntoReferencial.x;
+    puntoRotado.y=puntoRotado.y+puntoReferencial.y;
+    puntoRotado.z=puntoRotado.z+puntoReferencial.z;
+}
