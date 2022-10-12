fd4c43d27a20265587fc99ab3dc5a8b07ea3489c
Diego Rivas <drivash@unsa.edu.pe>
2022-10-11 20:29:43 -0500

    judge_488

---
+++ Set1/judge_488.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+void crearOnda(int,int);
+int main(){
+
+    int n;//Numero de casos
+    int amplitud,frecuencia;
+    string blanco;
+    cin>>n;
+    //cin>>blanco;
+    getline(cin,blanco);
+
+    for(int i=0;i<n;i++){
+        cin>>amplitud;
+        cin>>frecuencia;
+        crearOnda(amplitud,frecuencia);
+        //cin>>blanco;
+        cout<<endl;
+        getline(cin,blanco);
+    }
+    return 0;
+}
+void crearOnda(int amplitud,int frecuencia){
+    for(int f=0;f<frecuencia;f++){
+        for(int i=1;i<=amplitud;i++){
+            for(int j=1;j<=i;j++){
+                cout<<i;
+            }
+            cout<<endl;
+        }
+        for(int i=amplitud-1;i>=1;i--){
+            for(int j=i;j>=1;j--){
+                cout<<i;
+            }
+            cout<<endl;
+        }
+        cout<<endl;
+    }
+}
