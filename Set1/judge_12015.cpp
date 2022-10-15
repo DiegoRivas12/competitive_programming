d8bec650b755f80656281600c1d01405795ce9f6
Diego Rivas <drivash@unsa.edu.pe>
2022-10-15 17:12:36 -0500

    12015 y 12503 correct

--- Set1/judge_12015.cpp
+++ Set1/judge_12015.cpp
@@ -8,7 +8,7 @@ int main(){
     string auxURL;
     int auxV;
     cin>>T;
-    vector<vector<string>> casos(T);
+    //vector<vector<string>> casos(T);
     for(int i=0;i<T;i++){//Numeros de casos
         int mayor;
         string URL[10];
@@ -16,38 +16,37 @@ int main(){
         for(int j=0;j<10;j++){//10 paginas por cada caso
             cin>>auxURL;
             cin>>auxV;
-            if(1<=auxURL.size() && auxURL.size()<=100 && 1<=auxV && auxV<=100){
+            //if(1<=auxURL.size() && auxURL.size()<=100 && 1<=auxV && auxV<=100){
                 //cout<<"HAHAHAH"<<endl;
-                URL[j]=auxURL;
-                V[j]=auxV;
+            URL[j]=auxURL;
+            V[j]=auxV;
                 //cout<<"HAHAHAHsasa"<<endl;
                 //cout<<"url "<<URL[j]<<"v "<<V[j]<<endl;;
-            }
+            //}
         }
         //cout<<"urla "<<URL[9]<<"va "<<V[9]<<endl;;
         
         mayor=hallarMayor(V,10);//Pagina mas rankeada
+        
         //cout<<"Mayor "<<mayor<<endl;
+        
         //Escogiendo paginas rankeadas que coincidan en ser el mayor
-        for(int p=0;p<10;p++){
-            if(V[p]==mayor){
-                casos[i].push_back(URL[p]);
-            } 
+        cout<<"Caso #"<<i+1<<":"<<endl;
+        for(int k=0;k<10;k++){
+            if(V[k]==mayor){
+                cout<<URL[k]<<endl;
+            }
         }
+        
     }
     //Mostrando datos
-    for(int i=0;i<T;i++){
-        cout<<"Caso #"<<i+1<<":"<<endl;
-        for(int j=0;j<casos[i].size();j++){
-            cout<<casos[i][j]<<endl;
-        }
-    }
+    
     return 0;
 }
 
 int hallarMayor(int V[],int lon){
     int mayor=V[0];
-    for(int i=1;i<lon;i++){
+    for(int i=0;i<lon;i++){
         if(V[i]>mayor){
             mayor=V[i];
         }
