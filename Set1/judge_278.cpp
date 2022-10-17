4f6d84baca2026b25d5333e40166478c0b1a5a7d
Diego Rivas <drivash@unsa.edu.pe>
2022-10-16 19:18:34 -0500

    d

--- Set1/judge_278.cpp
+++ Set1/judge_278.cpp
@@ -1,10 +1,7 @@
-#include <bits/stdc++.h>
 #include<iostream>
 using namespace std;
-//#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
 
 int main(){// 4<=m,n<=10
-    //fastio
     int t, n, m;
     char letra;
     cin >> t;
@@ -23,7 +20,7 @@ int main(){// 4<=m,n<=10
             }
         } 
         else if(letra=='Q') {
-            //respuesta = min(n, m); //queen problem always have solution for n>=4
+            
             if(m<n){
                 respuesta=m;
             }
@@ -38,7 +35,7 @@ int main(){// 4<=m,n<=10
             respuesta = ((n+1)/2) * ((m+1)/2); // 1king take four cells
         } 
         else if(letra=='k') {//caballo
-           //respuesta = (n*m)%2 ? n*m/2+1 : n*m/2; // knight occupy one color
+          
            if((n*m)%2){
             respuesta=n*m/2+1;
            }
