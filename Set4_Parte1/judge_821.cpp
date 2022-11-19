8b64837cced4131f177f5f399f1099753f8fc5a4
Diego Rivas <drivash@unsa.edu.pe>
2022-11-19 10:58:28 -0500

    judge 821

--- Set4_Parte1/judge_821.cpp
+++ Set4_Parte1/judge_821.cpp
@@ -2,8 +2,34 @@
 using namespace std;
 int total=0;
 map<int,vector<int> >graph;
-int bfs(int src)
-{
+int bfs(int src);
+int main(){
+    int x,y,n1,n2,test=1;
+    while(scanf("%d %d",&x,&y) && (x||y)){
+        graph.clear();
+        set<int>st;
+        set<int>::iterator it;
+        graph[x].push_back(y);
+        st.insert(x);
+        st.insert(y);
+        while(scanf("%d %d",&n1,&n2) && (n1||n2))
+        {
+            graph[n1].push_back(n2);
+            st.insert(n1);
+            st.insert(n2);
+        }
+        double result=0.0;
+        for(it=st.begin();it!=st.end();it++)
+        {
+            result+=bfs(*it);
+        }
+        
+        printf("Case %d: average length between pages = %.3lf clicks\n",test++,result/total);
+        total=0;
+    }
+    return 0;
+}
+int bfs(int src){
     queue<int>Q;
     int level[100000];
     int visit[100000]={0};
@@ -28,34 +54,4 @@ int bfs(int src)
         }
     }
     return result;
-}
-int main()
-{
-//    freopen("in.txt","r",stdin);
-//    freopen("out.txt","w",stdout);
-    int x,y,n1,n2,test=1;
-    while(scanf("%d %d",&x,&y) && (x||y))
-    {
-        graph.clear();
-        set<int>st;
-        set<int>::iterator it;
-        graph[x].push_back(y);
-        st.insert(x);
-        st.insert(y);
-        while(scanf("%d %d",&n1,&n2) && (n1||n2))
-        {
-            graph[n1].push_back(n2);
-            st.insert(n1);
-            st.insert(n2);
-        }
-        double result=0.0;
-        for(it=st.begin();it!=st.end();it++)
-        {
-            result+=bfs(*it);
-        }
-        int ln=st.size();
-        printf("Case %d: average length between pages = %.3lf clicks\n",test++,result/total);
-        total=0;
-    }
-    return 0;
 }
